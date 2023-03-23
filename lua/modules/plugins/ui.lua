d92e855d120619c8ae8743e0139cde2b153e42e9
gaoyunfei <gaoyunfei@comisys.net>
2023-03-23 18:43:15 +0800

    delete diff

--- lua/modules/plugins/ui.lua
+++ lua/modules/plugins/ui.lua
@@ -1,64 +1,62 @@
 local ui = {}
 
 ui["goolord/alpha-nvim"] = {
-  lazy = true,
-  event = "BufWinEnter",
-  config = require("ui.alpha"),
+	lazy = true,
+	event = "BufWinEnter",
+	config = require("ui.alpha"),
 }
--- 使用lua为Neovim构建的时髦的💅缓冲线(带有tabpage集成)。 打开文档的文件名
 ui["akinsho/bufferline.nvim"] = {
-  lazy = true,
-  event = { "BufReadPost", "BufAdd", "BufNewFile" },
-  config = require("ui.bufferline"),
+	lazy = true,
+	event = { "BufReadPost", "BufAdd", "BufNewFile" },
+	config = require("ui.bufferline"),
 }
 ui["catppuccin/nvim"] = {
-  lazy = false,
-  name = "catppuccin",
-  config = require("ui.catppuccin"),
+	lazy = false,
+	name = "catppuccin",
+	config = require("ui.catppuccin"),
 }
 ui["sainnhe/edge"] = {
-  lazy = true,
-  config = require("ui.edge"),
+	lazy = true,
+	config = require("ui.edge"),
 }
--- nvm -lsp进程的独立界面。对不耐烦的人来说是花瓶。进度百分比
 ui["j-hui/fidget.nvim"] = {
-  lazy = true,
-  event = "BufReadPost",
-  config = require("ui.fidget"),
+	lazy = true,
+	event = "BufReadPost",
+	config = require("ui.fidget"),
 }
 ui["lewis6991/gitsigns.nvim"] = {
-  lazy = true,
-  event = { "CursorHold", "CursorHoldI" },
-  config = require("ui.gitsigns"),
+	lazy = true,
+	event = { "CursorHold", "CursorHoldI" },
+	config = require("ui.gitsigns"),
 }
 ui["lukas-reineke/indent-blankline.nvim"] = {
-  lazy = true,
-  event = "BufReadPost",
-  config = require("ui.indent-blankline"),
+	lazy = true,
+	event = "BufReadPost",
+	config = require("ui.indent-blankline"),
 }
 ui["nvim-lualine/lualine.nvim"] = {
-  lazy = true,
-  event = { "BufReadPost", "BufAdd", "BufNewFile" },
-  config = require("ui.lualine"),
+	lazy = true,
+	event = { "BufReadPost", "BufAdd", "BufNewFile" },
+	config = require("ui.lualine"),
 }
 ui["zbirenbaum/neodim"] = {
-  lazy = true,
-  event = "LspAttach",
-  config = require("ui.neodim"),
+	lazy = true,
+	event = "LspAttach",
+	config = require("ui.neodim"),
 }
 ui["karb94/neoscroll.nvim"] = {
-  lazy = true,
-  event = "BufReadPost",
-  config = require("ui.neoscroll"),
+	lazy = true,
+	event = "BufReadPost",
+	config = require("ui.neoscroll"),
 }
 ui["shaunsingh/nord.nvim"] = {
-  lazy = true,
-  config = require("ui.nord"),
+	lazy = true,
+	config = require("ui.nord"),
 }
 ui["rcarriga/nvim-notify"] = {
-  lazy = true,
-  event = "VeryLazy",
-  config = require("ui.notify"),
+	lazy = true,
+	event = "VeryLazy",
+	config = require("ui.notify"),
 }
 ui["folke/paint.nvim"] = {
 	lazy = true,
@@ -66,14 +64,14 @@ ui["folke/paint.nvim"] = {
 	config = require("ui.paint"),
 }
 ui["dstein64/nvim-scrollview"] = {
-  lazy = true,
-  event = "BufReadPost",
-  config = require("ui.scrollview"),
+	lazy = true,
+	event = "BufReadPost",
+	config = require("ui.scrollview"),
 }
 ui["edluffy/specs.nvim"] = {
-  lazy = true,
-  event = "CursorMoved",
-  config = require("ui.specs"),
+	lazy = true,
+	event = "CursorMoved",
+	config = require("ui.specs"),
 }
 
 return ui
